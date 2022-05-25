0e06afd68cdbe0948c1a5262e246c36144014f98
ekuzu <erinckuzu@gmail.com>
2022-05-25 16:54:55 +0300

    ADD: Tailwind support

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 import { AuthProvider } from './context/AuthContext'
 import { RouteList } from './routes'
+import './App.css'
 
 const App = () => (
   <BrowserRouter>
